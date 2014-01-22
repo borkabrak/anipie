87876646693851bbaf38b3514df9f54705e915b7
Jonathan Carter <jcarter@str.com>
2014-01-22 17:28:34 -0600

    It vaguely works, but something is real screwy with the math, I think.

--- piechart.js
+++ piechart.js
@@ -12,59 +12,56 @@ Raphael.fn.pieChart = function(data){
     'use strict';
 
     var me = this;
-    me.data = data;
+    me.data = data || {};
     me.sectors = me.set();
-    me.base = me.circle(200, 200, 100); 
-    me.total_value = Object.keys(me.data).map(function(e){ return me.data[e]}).reduce(function(x,y){ return x +y });
+    me.cx = 200;
+    me.cy = 200;
+    me.radius = 100; 
+    me.total_value = function(){
+        var values = Object.keys(me.data).map(function(x){ return me.data[x] });
+        var total = values.reduce(function(x,y){ return +x + +y });
+        log("Total: " + total);
+        return total;
+    };
 
     me.customAttributes.sector = function(startAngle, endAngle){
-        // Give angle in degrees
-        
-        var r = me.base.attrs.r,
-            cx = me.base.attrs.cx,
-            cy = me.base.attrs.cy,
-            flag = (endAngle - startAngle) > 180,
-            color = (endAngle - startAngle) / 360;
-
-        // convert to radians
+        // convert angles from degrees to radians
         startAngle = (startAngle % 360) * (Math.PI / 180);
         endAngle = (endAngle % 360) * (Math.PI / 180);
+        var flag = (endAngle - startAngle) > 180;
         
         return {   
             path:[ 
-                ["M", cx, cy],
-                ["l", r * Math.cos(startAngle), r * Math.sin(startAngle) ],
-                ["A", r, r, 0, +flag, 1, cx + r * Math.cos(endAngle), cy + r * Math.sin(endAngle)],
+                ["M", me.cx, me.cy],
+                ["l", me.radius * Math.cos(startAngle), me.radius * Math.sin(startAngle) ],
+                ["A", me.radius, me.radius, 0, +flag, 1, me.cx + me.radius * Math.cos(endAngle), me.cy + me.radius * Math.sin(endAngle)],
                 ["z"]
             ],
-            fill: "hsb(" + color + ", .75, .8)",
-            stroke: "white",
-            "stroke-width": "2px"
+            fill: "red"
         };
     };
 
-    // Draw sectors
-    me.draw = function(){
-        var startAngle = 0;
-        me.sectors.remove();
-        Object.keys(me.data).forEach(function(label){
-            var value = me.data[label];
-            var endAngle = startAngle + (360 / me.total_value * value);
-
-            me.sectors.push(me.path().attr({sector: [startAngle, endAngle]}));
-
-            startAngle = startAngle + endAngle;
-        });
-    };
-
-    // Add sector
     me.add_sector = function(label, value){
         log("add_sector(\"" + label + "\" ," + value + ")");
         me.data[label] = value;
-        me.draw();
+
+        // Iterate over sectors, recalculating each one's angles and animating to them.
+        // Then add the new sector.
+        var startAngle = 0, endAngle = 0;
+        me.sectors.forEach(function(sector){
+            endAngle = startAngle + (360 / +me.total_value() * +value);
+            log("sector from " + startAngle + " to " + endAngle);
+            sector.animate({sector: [startAngle, endAngle]}, 1000, "linear");
+            startAngle = endAngle;
+        });
+
+        endAngle = startAngle + (360 / me.total_value() * value);
+        me.sectors.push(
+            me.path().attr({sector: [startAngle, endAngle]})
+        );
+
     };
 
-    me.draw();
     return me;
 };
 
